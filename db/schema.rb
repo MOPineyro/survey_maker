6439f79110eda333ce9a90e1216e5197fef96e34
Manuel Omar Pineyro <student@epicodus.com>
2014-03-26 16:02:03 -0700

    Added models/tables for SurveyTaker, SurveySubmission. Survey now has many and belongs to surveytakers. Created join table surveytakers_surveys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326213426) do
+ActiveRecord::Schema.define(version: 20140326224832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,10 +37,28 @@ ActiveRecord::Schema.define(version: 20140326213426) do
     t.datetime "updated_at"
   end
 
+  create_table "survey_submissions", force: true do |t|
+    t.integer  "surveytaker_id"
+    t.integer  "survey_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "survey_takers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "surveys", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "surveytakers_surveys_tables", id: false, force: true do |t|
+    t.integer "survey_id"
+    t.integer "surveytaker_id"
   end
 
 end
